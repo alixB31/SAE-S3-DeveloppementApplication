6e57b9820ac2f6ea2eb4bde1e6e2ab4b5617ca92
Natou <122461007+Turlut@users.noreply.github.com>
2023-10-26 09:12:35 +0200

    classe question (en partie)

--- SAE_S3_DeveloppementApplication/src/modele/Question.java
+++ SAE_S3_DeveloppementApplication/src/modele/Question.java
@@ -1,5 +1,71 @@
 package modele;
 
 public class Question {
+	
+	/**/
+	public Categorie categorieQuestion;
 
+	/**/
+	public String intituleQuestion;
+	
+	/**/
+	public int difficulteQuestion;
+	
+	/**/
+	public String[] listeReponseFausse;
+	
+	/**/
+	public String reponseJuste;
+	
+	/**/
+	public String feedBackQuestion;
+	
+	/**
+	 * @param intitule
+	 * @param categorie
+	 * @param difficulte
+	 * @param liste
+	 * @param reponse
+	 * @param feedBack
+	 */
+	public Question(String intitule, Categorie categorie, int difficulte, 
+					String[] liste, String reponse, String feedBack) {
+		this.intituleQuestion = intitule;
+		this.categorieQuestion = categorie;
+		this.difficulteQuestion = difficulte;
+		this.listeReponseFausse = liste;
+		this.reponseJuste = reponse;
+		this.feedBackQuestion = feedBack;
+	}
+	
+	/**
+	 * @param nouvelleCategorie
+	 */
+	public boolean modifierCategorieQuestion(Categorie nouvelleCategorie) {
+		//TODO ecrire le corps...
+		return false;
+	}
+	
+	/**
+	 * @param nouvelleDifficulte
+	 */
+	public boolean modifierDifficulteQuestion(int nouvelleDifficulte) {
+		//TODO ecrire le corps...
+		return false;
+	}
+	
+	public boolean modifierReponseFausseQuestion(String[] nouvelleReponseFausse) {
+		//TODO ecrire le corps...
+		return false;
+	}
+	
+	public boolean modifierReponseJusteQuestion(String nouvelleReponseJuste) {
+		//TODO ecrire le corps...
+		return false;
+	}
+	
+	public boolean ajouterReponseFausse(String nouvelleReponseFausse) {
+		//TODO ecrire le corps...
+		return false;
+	}
 }
