385d723acc79fd3aec7c8ae5234e19f03b303ba0
Natou <122461007+Turlut@users.noreply.github.com>
2023-11-10 11:33:10 +0100

    gestions des erreurs du controleur

--- SAE_S3_DeveloppementApplication/src/controleur/ParametreController.java
+++ SAE_S3_DeveloppementApplication/src/controleur/ParametreController.java
@@ -12,20 +12,18 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.MenuItem; // Correction de l'import
-
-import javafx.scene.control.SplitMenuButton;
-import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import vue.Main;
 
-/** TODO comment class responsibility (SRP)
+/**
+ * TODO comment class responsibility (SRP)
+ * 
  * @author rayanibrahime
  * @author nathangirardin
  */
 public class ParametreController {
 
-	@FXML
+    @FXML
     private Button btnAjouter;
 
     @FXML
@@ -39,56 +37,82 @@ public class ParametreController {
 
     @FXML
     private Button btnSuivant;
-    
+
     @FXML
     private ComboBox<String> comboBox;
 
     @FXML
     void nouvelleCategorieAction(ActionEvent event) {
-        /* On crée l'interface pour ajouter une nouvelle catégorie à la liste déroulante */
-        TextInputDialog boiteSaisie = new TextInputDialog("");
-        boiteSaisie.setHeaderText("Ajouter une nouvelle catégorie");
-        boiteSaisie.setTitle("Catégorie");
-        boiteSaisie.setContentText("Entrez le nom de votre nouvelle catégorie: ");
-        boiteSaisie.showAndWait();
-        comboBox.getItems().add(boiteSaisie.getResult());
+        try {
+            TextInputDialog boiteSaisie = new TextInputDialog("");
+            boiteSaisie.setHeaderText("Ajouter une nouvelle catégorie");
+            boiteSaisie.setTitle("Catégorie");
+            boiteSaisie.setContentText("Entrez le nom de votre nouvelle catégorie: ");
+            boiteSaisie.showAndWait();
+
+            String result = boiteSaisie.getResult();
+            if (result != null && !result.isEmpty()) {
+                comboBox.getItems().add(result);
+            }
+        } catch (Exception e) {
+            // Gérer l'exception (afficher un message d'erreur, journaliser, etc.)
+            e.printStackTrace();
+        }
     }
-    
+
     @FXML
     void deleteCategorieAction(ActionEvent event) {
-    	/*
-    	 * Création d'une boîte d'alerte de type confirmation.
-    	 * Elle est dotée de 2 boutons : oui et non
-    	 */
-    	Alert boiteAlerte = new Alert(Alert.AlertType.CONFIRMATION,
-    			"Confirmez-vous votre choix ?",
-    			ButtonType.YES, ButtonType.NO);
+        try {
+            Alert boiteAlerte = new Alert(Alert.AlertType.CONFIRMATION, "Confirmez-vous votre choix ?",
+                    ButtonType.YES, ButtonType.NO);
 
-    	Optional<ButtonType> option = boiteAlerte.showAndWait();
-    	if (option.get() == ButtonType.YES) { // clic sur "oui"
-    		comboBox.getItems().remove(comboBox.getValue());
-    	}
+            Optional<ButtonType> option = boiteAlerte.showAndWait();
+            if (option.isPresent() && option.get() == ButtonType.YES) {
+                comboBox.getItems().remove(comboBox.getValue());
+            }
+        } catch (Exception e) {
+            // Gérer l'exception (afficher un message d'erreur, journaliser, etc.)
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void modifierCategorieAction(ActionEvent event) {
-    	String categorieCourante =  comboBox.getValue();
-    	TextInputDialog boiteSaisie = new TextInputDialog(categorieCourante);
-        boiteSaisie.setHeaderText("Modifier la catégorie : " + categorieCourante);
-        boiteSaisie.setTitle("Catégorie");
-        boiteSaisie.setContentText("Entrez le nouveau nom de votre catégorie : ");
-        boiteSaisie.showAndWait();
-        comboBox.getItems().set(comboBox.getItems().indexOf(categorieCourante),boiteSaisie.getResult());
+        try {
+            String categorieCourante = comboBox.getValue();
+            TextInputDialog boiteSaisie = new TextInputDialog(categorieCourante);
+            boiteSaisie.setHeaderText("Modifier la catégorie : " + categorieCourante);
+            boiteSaisie.setTitle("Catégorie");
+            boiteSaisie.setContentText("Entrez le nouveau nom de votre catégorie : ");
+            boiteSaisie.showAndWait();
+
+            String result = boiteSaisie.getResult();
+            if (result != null && !result.isEmpty()) {
+                comboBox.getItems().set(comboBox.getItems().indexOf(categorieCourante), result);
+            }
+        } catch (Exception e) {
+            // Gérer l'exception (afficher un message d'erreur, journaliser, etc.)
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void suivant(ActionEvent event) {
-    	Main.voirParamCategorie();
-    }
-    
-    @FXML
-    void retourMenu(ActionEvent event) {
-        Main.retourMenuPrincipal();
+        try {
+            Main.voirParamCategorie();
+        } catch (Exception e) {
+            // Gérer l'exception (afficher un message d'erreur, journaliser, etc.)
+            e.printStackTrace();
+        }
     }
 
+    @FXML
+    void retourMenu(ActionEvent event) {
+        try {
+            Main.retourMenuPrincipal();
+        } catch (Exception e) {
+            // Gérer l'exception (afficher un message d'erreur, journaliser, etc.)
+            e.printStackTrace();
+        }
+    }
 }
