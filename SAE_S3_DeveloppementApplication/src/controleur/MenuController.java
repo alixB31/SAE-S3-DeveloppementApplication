32d430199553ba7a917c0c65771b994e289fc6cd
Natou <122461007+Turlut@users.noreply.github.com>
2023-11-13 10:12:44 +0100

    ajout d'une icone joueur

--- SAE_S3_DeveloppementApplication/src/controleur/MenuController.java
+++ SAE_S3_DeveloppementApplication/src/controleur/MenuController.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextInputDialog;
 import vue.Main;
 
 /** TODO comment class responsibility (SRP)
@@ -26,6 +27,9 @@ public class MenuController {
 
     @FXML
     private Button btnQuitter;
+    
+    @FXML
+    private Button btnJoueur;
 
     @FXML
     void quitterQuiz(ActionEvent event) {
@@ -45,6 +49,16 @@ public class MenuController {
     @FXML 
     void lancerParam(ActionEvent event) {
        Main.lancerParametre();
+    }
+    
+    @FXML
+    void btnJoueurAction(ActionEvent event) {
+    	/*on crée l'interface pour modifier l'intitulé d'une question*/
+        TextInputDialog boiteSaisie = new TextInputDialog(""); 
+        boiteSaisie.setHeaderText("Pseudo du Joueur"); 
+        boiteSaisie.setTitle("Joueur"); 
+        boiteSaisie.setContentText("Pseudo du joueur : ");
+        boiteSaisie.showAndWait();
     }
     
 }
