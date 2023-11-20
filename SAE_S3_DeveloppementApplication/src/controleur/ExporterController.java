e9d94190bc3152e2babb91930034dc1c04a619e3
alixB31 <131276536+alixB31@users.noreply.github.com>
2023-11-20 15:12:56 +0100

    garde les questions qu'on a sélectionné de coté

--- SAE_S3_DeveloppementApplication/src/controleur/ExporterController.java
+++ SAE_S3_DeveloppementApplication/src/controleur/ExporterController.java
@@ -34,11 +34,12 @@ public class ExporterController {
 
 	@FXML
 	private VBox vBoxQuestions;
-	
+
 	private boolean allSelected = false;
-	
+
 	private String CategorieSelection;
 
+	private ArrayList<String> questionCheck = new ArrayList<>();
 	public VBox vBox = new VBox();
 
 	@FXML
@@ -51,47 +52,76 @@ public class ExporterController {
 
 		allSelected = !allSelected;
 
-        for (javafx.scene.Node node : vBox.getChildren()) {
-            if (node instanceof CheckBox) {
-                CheckBox checkBox = (CheckBox) node;
-                checkBox.setSelected(allSelected);
-            }
-        }
+		for (javafx.scene.Node node : vBox.getChildren()) {
+			if (node instanceof CheckBox) {
+//				CheckBox checkBox = (CheckBox) node;
+//				checkBox.setSelected(allSelected);
+				CheckBox checkBox = (CheckBox) node;
+	            checkBox.setSelected(allSelected);
+
+	            // Si la CheckBox est cochée, l'ajouter à la liste
+	            if (checkBox.isSelected()) {
+	            	checkBox.setOnAction(event2 -> handleCheckBoxAction(checkBox));
+	            }
+			}
+		}
 	}
 
-		@FXML
-		void comboBoxCategorieAction(ActionEvent event) {
+	@FXML
+	void comboBoxCategorieAction(ActionEvent event) {
 
-			//clear la liste des questions affichés
+		//clear la liste des questions affichés
 
-			vBox.getChildren().clear();
-			CategorieSelection = comboBoxCategorie.getValue();
-			HashMap<String, Categorie> listeCategorie = Main.stockage.getListeCategorie();
+		vBox.getChildren().clear();
+		CategorieSelection = comboBoxCategorie.getValue();
+		HashMap<String, Categorie> listeCategorie = Main.stockage.getListeCategorie();
 
-			//cas ou l'on veut toutes les questions de toutes les catégories
-			if (CategorieSelection == "Toutes les catégories") {
-				for (Categorie categorie : listeCategorie.values()) {
-					ArrayList<Question> listeQuestion = Main.stockage.listeQuestionParCategorie((Categorie)Main.stockage.getListeCategorie().get(categorie.getIntituleCategorie()));
-					for (int y = 0; y<listeQuestion.size(); y++) {
-						CheckBox dynamicCheckBox = new CheckBox(listeQuestion.get(y).getIntituleQuestion());
-						vBox.getChildren().add(dynamicCheckBox);
-					}
+		//cas ou l'on veut toutes les questions de toutes les catégories
+		if (CategorieSelection == "Toutes les catégories") {
+			for (Categorie categorie : listeCategorie.values()) {
+				ArrayList<Question> listeQuestion = Main.stockage.listeQuestionParCategorie((Categorie)Main.stockage.getListeCategorie().get(categorie.getIntituleCategorie()));
+				for (int y = 0; y<listeQuestion.size(); y++) {
+					CheckBox dynamicCheckBox = new CheckBox(listeQuestion.get(y).getIntituleQuestion());
+					dynamicCheckBox.setOnAction(event2 -> handleCheckBoxAction(dynamicCheckBox));
+					 // Cocher seulement la CheckBox correspondant au catégorie déja checké
+				    if (questionCheck.contains(listeQuestion.get(y).getIntituleQuestion())) {
+				        dynamicCheckBox.setSelected(true);
+				    }
+					vBox.getChildren().add(dynamicCheckBox);
 				}
-
-			} 
-
-			ArrayList<Question> listeQuestionParCategorie = Main.stockage.listeQuestionParCategorie((Categorie)Main.stockage.getListeCategorie().get(CategorieSelection));
-			for (int i = 0; i<listeQuestionParCategorie.size(); i++) {
-				CheckBox dynamicCheckBox = new CheckBox(listeQuestionParCategorie.get(i).getIntituleQuestion());
-				vBox.getChildren().add(dynamicCheckBox);
 			}
 
-			scrollBarContenu.setContent(vBox);
+		} 
+
+		//cas ou l'on veut que les questions d'une catégorie
+		ArrayList<Question> listeQuestionParCategorie = Main.stockage.listeQuestionParCategorie((Categorie)Main.stockage.getListeCategorie().get(CategorieSelection));
+		for (int i = 0; i<listeQuestionParCategorie.size(); i++) {
+			CheckBox dynamicCheckBox = new CheckBox(listeQuestionParCategorie.get(i).getIntituleQuestion());
+			dynamicCheckBox.setOnAction(event2 -> handleCheckBoxAction(dynamicCheckBox));
+			if (questionCheck.contains(listeQuestionParCategorie.get(i).getIntituleQuestion())) {
+		        dynamicCheckBox.setSelected(true);
+		    }
+			vBox.getChildren().add(dynamicCheckBox);
+			// Gestionnaire d'événements pour chaque CheckBox
+			
+
 		}
 
-		@FXML
-		void btnExporterAction(ActionEvent event) {
-			Main.ihmEnvoie();
-		}
+		scrollBarContenu.setContent(vBox);
 
 	}
+
+	// Méthode pour gérer les événements de CheckBox
+	private void handleCheckBoxAction(CheckBox checkBox) {
+		if (checkBox.isSelected()) {
+			questionCheck.add(checkBox.getText());
+		} else {
+			questionCheck.remove(checkBox.getText());
+		}
+	}
+	@FXML
+	void btnExporterAction(ActionEvent event) {
+		Main.ihmEnvoie();
+	}
+
+}
