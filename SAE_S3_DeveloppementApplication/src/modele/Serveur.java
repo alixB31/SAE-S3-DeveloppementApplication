10bc5cf325d75b69c2285cf3871943a2232f1ac9
Bratoun <rayan.ibrahime@iut-rodez.fr>
2023-11-22 14:55:59 +0100

    serveur

--- SAE_S3_DeveloppementApplication/src/modele/Serveur.java
+++ SAE_S3_DeveloppementApplication/src/modele/Serveur.java
@@ -23,40 +23,42 @@ public class Serveur {
 	
 	public final static int NUM_PORT =  12345;
 	private static ServerSocket serverSocket;
-	
+	private static Socket clientSocket;
 	public static boolean gererConnexion() {
+		boolean Ok;
+		Ok = true;
 	    try {
 	        // Création d'un ServerSocket écoutant sur le port 12345
 	        serverSocket = new ServerSocket(NUM_PORT);
 
 	        // Définir un temps d'attente maximal en millisecondes
-	        serverSocket.setSoTimeout(8000);
+	        serverSocket.setSoTimeout(2000);
 
 	        System.out.println("Attente de connexion...");
-
+	       
 	        try {
 	            //Attente d'une connexion cliente
-	            Socket clientSocket = serverSocket.accept();
+	            clientSocket = serverSocket.accept();
 
 	            System.out.println("Connexion établie.");
 
 	            // Appel de la méthode pour traiter la réception du fichier
-	            traiterReceptionFichier(clientSocket);
-
+	            traiterReceptionFichier();
+	            
 	            // Close the ServerSocket here if needed
 	            serverSocket.close();
-
+	           
 	        } catch (java.net.SocketTimeoutException e) {
 	            System.out.println("Temps d'attente écoulé. Aucune connexion reçue dans le délai spécifié.");
 	            // Close the ServerSocket after the timeout
 	            serverSocket.close();
-	            return false;
+	            Ok = false;
 	        }
 
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
-	    return true;
+	    return Ok;
 	}
 	
 	
@@ -67,13 +69,15 @@ public class Serveur {
      * @param clientSocket Le socket du client.
      * @throws IOException En cas d'erreur d'entrée/sortie.
      */
-    private static void traiterReceptionFichier(Socket clientSocket) throws IOException {
+	private static File receivedFile;
+	private static FileOutputStream fileOut;
+    private static void traiterReceptionFichier() throws IOException {
         // Obtention du flux d'entrée du client
         BufferedInputStream in = new BufferedInputStream(clientSocket.getInputStream());
 
         // Création d'un fichier pour stocker le fichier CSV reçu
-        File receivedFile = new File("received.csv");
-        FileOutputStream fileOut = new FileOutputStream(receivedFile);
+        receivedFile = new File("received.csv");
+        fileOut = new FileOutputStream(receivedFile);
 
         // Lecture et écriture du fichier
         byte[] buffer = new byte[1024];
@@ -81,13 +85,17 @@ public class Serveur {
         while ((bytesRead = in.read(buffer)) != -1) {
             fileOut.write(buffer, 0, bytesRead);
         }
-
+        
         System.out.println("Fichier reçu et enregistré : " + receivedFile.getAbsolutePath());
-
         // Fermeture des flux et du socket
         in.close();
         fileOut.close();
         clientSocket.close();
+        
+    }
+    
+    public static String getNomFichier() {
+    	return receivedFile.getAbsolutePath();
     }
     
     public static void fermerServeur() throws IOException {
