7c7715566e883eec76de642af8f33a68fb28f063
Natou <122461007+Turlut@users.noreply.github.com>
2023-11-28 10:47:16 +0100

    Update TestImportation.java

--- SAE_S3_DeveloppementApplication/src/tests/TestImportation.java
+++ SAE_S3_DeveloppementApplication/src/tests/TestImportation.java
@@ -3,9 +3,11 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import modele.Categorie;
+import modele.Client;
 import modele.ListeCategorie;
 import modele.ListeQuestion;
 import modele.Question;
+import modele.Serveur;
 import modele.Stockage;
 import vue.Main;
 
@@ -36,34 +38,66 @@ class TestImportation {
 //        assertTrue(!questionsMap.isEmpty());
 //
 //    }
+//    
+//    @Test
+//    public void testExportCSV() {
+//    	ArrayList<String> reponsesFausses = new ArrayList<>();
+//    	reponsesFausses.add("Faux1");
+//    	reponsesFausses.add("Faux2");
+//
+//        
+//        Categorie categorie1 = new Categorie("Cat1");
+//        Categorie categorie2 = new Categorie("Cat2");
+//    	
+//        // Créer des objets Question pour tester
+//        Question question1 = new Question("Question 1", categorie1, 1, reponsesFausses, "ReponseJuste1", "");
+//        Question question2 = new Question("Question 2", categorie2, 1, reponsesFausses, "ReponseJuste1", "Feedback1");
+//
+//        // Ajouter les questions à une liste
+//        ArrayList<Question> listeQuestions = new ArrayList<>();
+//        listeQuestions.add(question1);
+//        listeQuestions.add(question2);
+//
+//        // Créer une instance de votre classe
+//        Stockage stockage = new Stockage();
+//
+//        // Appeler la méthode exportCSV avec la liste de questions
+//        boolean estExporte = stockage.exportCSV(listeQuestions);
+//
+//        // Vérifier que l'exportation a réussi
+//        assertTrue(estExporte);
+//    }
     
-    @Test
-    public void testExportCSV() {
-    	ArrayList<String> reponsesFausses = new ArrayList<>();
-    	reponsesFausses.add("Faux1");
-    	reponsesFausses.add("Faux2");
+	@Test
+    public void testCommunication() {
+        Thread serverThread = new Thread(() -> {
+            assertTrue(Serveur.gererConnexion());
+        });
+        serverThread.start();
 
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        boolean estEnvoye = Client.envoyer("localhost");
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         
-        Categorie categorie1 = new Categorie("Cat1");
-        Categorie categorie2 = new Categorie("Cat2");
-    	
-        // Créer des objets Question pour tester
-        Question question1 = new Question("Question 1", categorie1, 1, reponsesFausses, "ReponseJuste1", "");
-        Question question2 = new Question("Question 2", categorie2, 1, reponsesFausses, "ReponseJuste1", "Feedback1");
+        Serveur.arreterConnexion();
 
-        // Ajouter les questions à une liste
-        ArrayList<Question> listeQuestions = new ArrayList<>();
-        listeQuestions.add(question1);
-        listeQuestions.add(question2);
+        try {
+            serverThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        // Créer une instance de votre classe
-        Stockage stockage = new Stockage();
-
-        // Appeler la méthode exportCSV avec la liste de questions
-        boolean estExporte = stockage.exportCSV(listeQuestions);
-
-        // Vérifier que l'exportation a réussi
-        assertTrue(estExporte);
+        assertTrue(estEnvoye);
     }
 }
 
